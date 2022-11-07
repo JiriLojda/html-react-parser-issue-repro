1606389317fd2b446e77bf9e79f3343cb303c4e3
Jiri Lojda <jiril@kontent.ai>
2022-11-07 18:34:19 +0100

    Create sample code using html-react-parser that doesn't replace elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import parseHTML, { DOMNode, Element } from 'html-react-parser';
 import './App.css';
+
+const replacer = (domNode: DOMNode) => {
+  console.log('is instance of?: ', domNode instanceof Element);
+  if (domNode instanceof Element && domNode.attribs['id'] === 'remove') {
+    console.log('replacing...');
+    return <></>;
+  }
+  return undefined;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {parseHTML('<div>stay and remove -> <b id="remove">This should be removed</b></div>', {
+          replace: replacer,
+        })}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
